670b918ae9292f1a399af7c8aec882c48eb9656a
yusufsakr <yusuf.sakr.eng@gmail.com>
2023-12-13 22:21:40 +0200

    Assignment 1

---
+++ finder-app/finder.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+filesdir=$1
+searchstr=$2
+
+if [ $# != 2 ]
+then
+    echo "Lack of Arguments"
+    exit 1
+elif [ -d "$filesdir" ]
+then    
+    files_count=$(ls -r $filesdir | wc -l)
+    lines_count=$(grep "$searchstr"* $filesdir -r  | wc -l)
+    echo "The number of files are $files_count and the number of matching lines are $lines_count"
+    exit 0
+else
+    echo "Directory not found"
+    exit 1
+fi
---
+++ finder-app/writer.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+writefile=$1
+writestr=$2
+
+dir=$(dirname "$writefile")
+filename=$(basename "$writefile")
+
+if [ $# != 2 ]
+then
+    echo "Lack of Arguments"
+    exit 1
+fi
+
+if [ -d "$dir" ]
+then    
+    sudo touch $writefile
+
+    if [ $? -eq 1 ]
+    then 
+        echo "Could not Create the File 1"
+        exit 1
+    fi
+
+    sudo chmod 777 $writefile 
+    echo $writestr > $writefile 
+    exit 0
+else
+    mkdir $dir
+
+    if [ $? -eq 1 ]
+    then 
+        echo "Could not Create the Directory"
+        exit 1
+    fi
+
+    sudo touch $writefile
+
+    if [ $? -eq 1 ]
+    then 
+        echo "Could not Create the File"
+        exit 1
+    fi
+
+    sudo chmod 777 $writefile 
+    echo $writestr > $writefile 
+    exit 0
+fi
